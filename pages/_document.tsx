2ea19f99e8a16cc50bd30c832edb8a6ef216773b
hwangseong-in <gommpo111@gmail.com>
2019-08-31 17:41:51 +0900

    fix pages component call getInitialProps

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,11 @@
-import Document, {Head, Main, NextScript} from 'next/document'
+import Document, { Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
+
+    static async getInitialProps(ctx) {
+        const initialProps = await Document.getInitialProps(ctx)
+        return { ...initialProps }
+    }
 
     render() {
         return <html>
