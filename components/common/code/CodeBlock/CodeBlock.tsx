909dd14c11a01c164b5fa1632f55a4eba8cecb51
hwangseong-in <gommpo111@gmail.com>
2019-09-04 15:27:12 +0900

    fix codeBlock bug

--- components/common/code/CodeBlock/CodeBlock.tsx
+++ components/common/code/CodeBlock/CodeBlock.tsx
@@ -5,20 +5,22 @@ import hljs from 'highlight.js';
 interface CodeBlockProps {
   value: string;
   language: string;
+  codeEl: HTMLElement;
 }
 
 const CodeBlock: React.FC<CodeBlockProps> = ({
                                                value,
+                                               codeEl,
                                                language,
                                              }) => {
 
   useEffect(() => {
     highlightCode();
-  });
+  }, [value, codeEl, language]);
 
-  const setRef = (el: HTMLElement) => this['codeEl'] = el;
+  const setRef = (el: HTMLElement) => codeEl = el;
 
-  const highlightCode = () => hljs.highlightBlock(this.codeEl);
+  const highlightCode = () => hljs.highlightBlock(codeEl);
 
   return (
     <pre>
