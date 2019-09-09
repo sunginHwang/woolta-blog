2ffdc15847bd6a1dd61e8570b98583dc3dd721df
hwangseong-in <gommpo111@gmail.com>
2019-09-09 15:15:47 +0900

    make dom access util function

---
+++ core/utils/domUtil.ts
@@ -0,0 +1,5 @@
+export const getElements = selector => document.querySelectorAll(selector);
+export const getElement = selector => document.querySelector(selector);
+export const addClass = (element, className) => element.classList.add(className);
+export const removeClass = (element, className) => element.classList.remove(className);
+export const hasClass = (element, className) => element.classList.contains(className);
