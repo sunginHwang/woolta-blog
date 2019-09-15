34697fa97c0b3e3f4f0848fc48b60b7cc0351e4a
hwangseong-in <gommpo111@gmail.com>
2019-09-15 19:26:51 +0900

    split container

---
+++ containers/WriteViewContainer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+interface WriteViewContainerProps{
+}
+
+const WriteViewContainer: React.FC<WriteViewContainerProps> = ({}) => {
+ return (
+  <div>
+   
+  </div>
+ );
+};
+
+export default WriteViewContainer;
