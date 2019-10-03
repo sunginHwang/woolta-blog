e628610bdec95ea05771c9bdcf63082b8b5748e4
hwangseong-in <gommpo111@gmail.com>
2019-10-03 19:22:54 +0900

    overriding nextPage type

--- types/next/NextPageCustom.ts
+++ types/next/NextPageCustom.ts
@@ -1,6 +1,47 @@
+import { IncomingMessage, ServerResponse } from 'http';
+import { ParsedUrlQuery } from 'querystring';
+import { AppType } from 'next-server/dist/lib/utils';
+
 export type NextPageCustom<P = {}, IP = P> = {
   (props: P): JSX.Element | null
   defaultProps?: Partial<P>
   displayName?: string
-  getInitialProps?(ctx: any): Promise<IP>
+  getInitialProps?(ctx: nextPageProps): Promise<IP>
+}
+
+interface nextPageProps {
+  /**
+   * Error object if encountered during rendering
+   */
+  err?: Error & {
+    statusCode?: number;
+  } | null;
+  /**
+   * `HTTP` request object.
+   */
+  req?: IncomingMessage;
+  /**
+   * `HTTP` response object.
+   */
+  res?: ServerResponse;
+  /**
+   * Path section of `URL`.
+   */
+  pathname: string;
+  /**
+   * Query string section of `URL` parsed as an object.
+   */
+  query: ParsedUrlQuery;
+  /**
+   * `String` of the actual path including query.
+   */
+  asPath?: string;
+  /**
+   * `Component` the tree of the App to use if needing to render separately
+   */
+  AppTree: AppType;
+
+  // 커스텀 항목 추가
+  store: any;
+  isServer: boolean;
 }
