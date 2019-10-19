ec6bacd8fd4055937a743da7b8ed9602cd88e1f3
hwangseong-in <gommpo111@gmail.com>
2019-10-20 00:39:04 +0900

    add pushApi

---
+++ core/api/pushApi.ts
@@ -0,0 +1,11 @@
+import apiCall from '../utils/apiCall';
+import { BLOG_API } from '../constants';
+import { IPwaSubscription } from '../../types/pwa/IPwaSubscription';
+
+export const pushSubscription = (subscription: IPwaSubscription) => {
+  apiCall.post(`${BLOG_API}/push/subscription`, {
+    auth: subscription.keys.auth,
+    key: subscription.keys.p256dh,
+    endPoint: subscription.endpoint,
+  });
+};
