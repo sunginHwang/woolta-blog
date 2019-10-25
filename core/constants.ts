ce1b4f1a7c32fca477d14297c23872f4d17925bf
hwangseong-in <gommpo111@gmail.com>
2019-10-25 18:46:55 +0900

    add constants

--- core/constants.ts
+++ core/constants.ts
@@ -1,10 +1,18 @@
 /*토큰 상수*/
 export const ACCESS_TOKEN: string = '_WOOLTA_USER_';
+export const ACCESS_PUSH_TOKEN: string = '_WOOLTA_PUSH__';
+export const PUSH_APPLICATION_SERVER_KEY = 'BBUmILImgSCb6wcUMIDPKj1B-kxu_x4VtHeQYVkLIRAlFCtTTFblcRsANxQCBfBYR8jOSx4OsvoFjObsyWc5p9Y';
 export const ACCESS_HEADER_TOKEN: string = 'Authorization';
 export const BLOG_API: string = 'http://localhost:8090';
 export const IMAGE_API: string = 'https://image.woolta.com';
 export const BLOG_URL: string = 'https://blog.woolta.com';
 export const TEMP_POST_AUTO_SAVE: string = 'TEMP_POST_AUTO_SAVE';
 export const BLOG_THUMBNAIL_IMAGE_URL: string = `${IMAGE_API}/3fed2d102ca753c6.png`;
+export const PWA_NOTIFICATION_PERMISSIONS = {
+  default: 'default',
+  granted: 'granted',
+  denied: 'denied',
+}
+export const PWA_LOG: string = '[WOOLTA_BLOG]';
 
 export const FIVE_MIN:number = 1000 * 60 * 5;
