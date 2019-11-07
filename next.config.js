5b483628a21b43ef074a0184427cee9ff80653d6
hwangseong-in <gommpo111@gmail.com>
2019-11-07 18:51:51 +0900

    setting deploy config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,27 @@
 const withSass = require('@zeit/next-sass');
-module.exports = withSass({
-  cssModules: true,
-  cssLoaderOptions: {
-    importLoaders: 1,
-    localIdentName: '[local]___[hash:base64:5]',
-  },
-});
+const compose = require('next-compose');
+const withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
+
+module.exports = compose([
+  [withBundleAnalyzer,{
+    analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+    analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
+    bundleAnalyzerConfig: {
+      server: {
+        analyzerMode: 'static',
+        reportFilename: '../bundles/server.html'
+      },
+      browser: {
+        analyzerMode: 'static',
+        reportFilename: '../bundles/client.html'
+      }
+    }
+  }],
+  [withSass, {
+    cssModules: true,
+    cssLoaderOptions: {
+      importLoaders: 1,
+      localIdentName: '[local]___[hash:base64:5]',
+    },
+  }],
+]);
