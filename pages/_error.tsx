df3865e1882c988c9e5cfdc822fb769b4f427bc7
hwangseong-in <gommpo111@gmail.com>
2019-11-09 23:28:53 +0900

    setup error page

---
+++ pages/_error.tsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+export default class Error extends Component {
+  static getInitialProps({ res, err }) {
+    const statusCode = res ? res.statusCode : err ? err.statusCode : null;
+    return { statusCode }
+  }
+
+  findErrorPage(statusCode){
+    switch (statusCode){
+      default: return <div>새로고침 해서 다시 시도해 주세요....</div>;
+    }
+  }
+
+  render() {
+    // @ts-ignore
+    const { statusCode } = this.props;
+    const { findErrorPage } = this;
+
+    let errorComponent = findErrorPage(statusCode);
+
+    return (
+      <div>
+        {errorComponent}
+      </div>
+    )
+  }
+}
