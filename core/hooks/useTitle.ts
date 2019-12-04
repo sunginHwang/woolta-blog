08d4cf929e59a47110a8f4bd40468a8fb5ea2d1d
hwangseong-in <gommpo111@gmail.com>
2019-12-04 14:39:16 +0900

    fix useTitle add bug

--- core/hooks/useTitle.ts
+++ core/hooks/useTitle.ts
@@ -3,18 +3,20 @@ import { addElement, getElement } from '../utils/domUtil';
 
 const updateTitle = (title: string) => {
   const titleElement = getElement('title');
+
   if (titleElement) {
     titleElement.innerText = title;
   } else {
-    const title = addElement('title');
-    title.innerText = title;
-    document.head.appendChild(title);
+    const newTitleElement = addElement('title');
+    newTitleElement.innerText = title;
+    document.head.appendChild(newTitleElement);
   }
 
 };
 
 export default function useTitle(initTitle?: string) {
-  const [title, setTitle] = useState(initTitle);
+
+  const [title, setTitle] = useState(initTitle ? initTitle : 'woolta blog');
   useEffect(() => updateTitle(title), [title]);
   return setTitle;
 };
