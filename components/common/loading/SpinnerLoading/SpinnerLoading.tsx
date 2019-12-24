9c190afb4e3e6230b0bd526f134f675650afc788
hwangseong-in <gommpo111@gmail.com>
2019-12-24 17:32:37 +0900

    change component style css-in-js

--- components/common/loading/SpinnerLoading/SpinnerLoading.tsx
+++ components/common/loading/SpinnerLoading/SpinnerLoading.tsx
@@ -1,24 +1,50 @@
-import { HashLoader } from 'react-spinners';
 import React from 'react';
-import classNames from 'classnames/bind';
-import cn from './SpinnerLoading.scss';
+import styled from 'styled-components';
+import { HashLoader } from 'react-spinners';
 
-const cx = classNames.bind(cn);
-
-interface SpinnerLoadingProps {
-  loading: boolean
+type SpinnerLoadingProps = {
+  loading: boolean;
 }
 
-const SpinnerLoading = ({ loading }: SpinnerLoadingProps) => (
-  loading ?
-    <div className={cx(cn.spinnerLoading)}>
-      <div className={cx(cn.spinnerLoading__position)}>
+function SpinnerLoading({ loading }: SpinnerLoadingProps) {
+
+  return (
+    <S.SpinnerLoading>
+      <div>
         <HashLoader
           color={'#6e827f'}
           loading={loading}/>
       </div>
-    </div>
-    : null
-);
+    </S.SpinnerLoading>
+  );
+};
 
 export default SpinnerLoading;
+
+const S: any = {};
+
+S.SpinnerLoading = styled.div`
+  background-color: #e8e8e8;
+  z-index: 1000;
+  position: fixed;
+  top: 0!important;
+  left: 0!important;
+  width: 100%;
+  height: 100%;
+  vertical-align: middle;
+  background-color: rgba(0,0,0,.2);
+  
+    div{
+      z-index: 1001;
+    
+      position: fixed;
+      left: 50%;
+      top: 50%;
+    
+      -webkit-transform: translate(-50%, -50%);
+      -ms-transform: translate(-50%, -50%);
+      -moz-transform: translate(-50%, -50%);
+      -o-transform: translate(-50%, -50%);
+      transform: translate(-50%, -50%);
+    }
+  `;
